304313b5300658a2cfa263efc0bd4273242905ef
Maks Kozub <maks.kozub@gmail.com>
2025-05-08 14:04:30 +0200

    fixed t2 and did task 3

--- Tutorial8/Controllers/ClientController.cs
+++ Tutorial8/Controllers/ClientController.cs
@@ -1,10 +1,11 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Tutorial8.Models;
 using Tutorial8.Services;
 
 namespace Tutorial8.Controllers
 {
-    [Route("api/clients/[id]/trips")]
+    [Route("api/[controller]")]
     [ApiController]
     public class ClientController : ControllerBase
     {
@@ -21,6 +22,28 @@ namespace Tutorial8.Controllers
             var trips = await _clientsService.getClientTripsByIdAsync(id, cancellationToken);
             
             return Ok(trips);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateClient([FromBody] Client newClient, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrEmpty(newClient.FirstName) ||
+                string.IsNullOrEmpty(newClient.LastName) ||
+                string.IsNullOrEmpty(newClient.Email) ||
+                string.IsNullOrEmpty(newClient.Pesel) ||
+                string.IsNullOrEmpty(newClient.Telephone))
+            {
+                return BadRequest();
+            }
+            
+            var newClientId = await _clientsService.CreateClientAsync(newClient, cancellationToken);
+
+            if (newClientId is null)
+            {
+                return NotFound();
+            }
+            
+            return Created(string.Empty, new { IdClient = newClientId });
         }
     }
 }
